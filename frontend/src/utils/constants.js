2dec8ff4a8704ef266b20d94cadac8a6c7712230
VickyGirase <vickygirase3908@gmail.com>
2025-09-30 11:50:16 +0530

    minor changes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:5000";
 
-export const BASE_URL = "https://devmeetx-12.onrender.com";
+export const BASE_URL = "https://devmeetx-apis.onrender.com";
 
 
 // "https://devmeetx-12.onrender.com"

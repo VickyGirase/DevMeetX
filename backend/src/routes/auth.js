adb93ed71423f058548385465c3130f738f8e772
VickyGirase <vickygirase3908@gmail.com>
2025-09-30 12:06:42 +0530

    minor bug solved

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -5,6 +5,16 @@ const { validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
+// Cookie options: in production we must use SameSite=None and Secure for cross-site cookies
+const isProd = process.env.NODE_ENV === "production";
+const cookieOptions = {
+  httpOnly: true,
+  // 8 hours expiry
+  expires: new Date(Date.now() + 8 * 3600000),
+  secure: isProd, // required for SameSite=None
+  sameSite: isProd ? "None" : "Lax",
+};
+
 authRouter.post("/signup", async (req, res) => {
   try {
     
@@ -25,9 +35,7 @@ authRouter.post("/signup", async (req, res) => {
     const savedUser = await user.save();
     const token = await savedUser.getJWT();
 
-    res.cookie("token", token, {
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
+    res.cookie("token", token, cookieOptions);
 
     res.json({ message: "User Added successfully!", data: savedUser });
   } catch (err) {
@@ -48,9 +56,7 @@ authRouter.post("/login", async (req, res) => {
     if (isPasswordValid) {
       const token = await user.getJWT();
 
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000),
-      });
+      res.cookie("token", token, cookieOptions);
       res.send(user);
     } else {
       throw new Error("Invalid credentials");
@@ -61,9 +67,7 @@ authRouter.post("/login", async (req, res) => {
 });
 
 authRouter.post("/logout", async (req, res) => {
-  res.cookie("token", null, {
-    expires: new Date(Date.now()),
-  });
+  res.cookie("token", null, { ...cookieOptions, expires: new Date(Date.now()) });
   res.send("Logout Successful!!");
 });
 
